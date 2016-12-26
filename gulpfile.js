4e2ea773374ae345e6eed7da7fe9e636cc1f7482
Bryan Cash <bcash0578@gmail.com>
2016-12-26 00:50:57 -0500

    Gulp Workflow CSS
    Installed sass and css plugins to compile sass and optimize css for
    production. Plugins include
    - gulp-sass
    - gulp-postcss
    - autoprefixer
    - precss
    - cssnano
    Created a task to compile all sass files into css files in the
    development folder then created a task to optimize all css files and
    send them into the production folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,51 @@
+/*
+Gulp Workflow
+* Make sure Node is installed or install Node
+* Run npm init to create pakage.json
+* Install Gulp npm install --save-dev gulp
+* Install plugins for workflow
+* browser-sync, gulp-sass, cssnano
+*/
+// Install gulp plugins
 var gulp = require('gulp'),
-	browserSync = require('browser-sync');
+	browserSync = require('browser-sync'),
+	sass = require('gulp-sass'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	precss = require('precss'),
+	cssnano = require('cssnano');
 
-//Development server to watch and update all HTML, CSS, images and js files
+// HTML
+gulp.task('html', function() {
+	gulp.src('development/*.html')
+	.pipe(gulp.dest('dist/'))
+});
+// Compile Sass into CSS
+gulp.task('sass', function() {
+	gulp.src('development/scss/**/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('development/css'))
+});
+
+// optimize css and send it to production
+gulp.task('css', function() {
+    gulp.src('development/css/**/*.css')
+    .pipe(postcss([
+        precss(),
+        autoprefixer(),
+        cssnano()
+    ]))
+    .pipe(gulp.dest('dist/css'))
+});
+
+// Watch Files for changes
+gulp.task('watch', function() {
+	gulp.watch('development/**/*.html')
+	gulp.watch('development/scss/**/*.scss')
+	gulp.watch('development/css/**/*.css')
+});
+
+// Development server to watch and update all HTML, CSS, images and js files
 gulp.task('browser-sync', function () {
    var files = [
       'development/**/*.html',
@@ -17,6 +61,4 @@ gulp.task('browser-sync', function () {
    });
 });
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+gulp.task('default', ['watch', 'html', 'sass', 'css']);
