7316308e65d9c84ceda1fb1672c6e2e770174ff4
Bryan Cash <bcash0578@gmail.com>
2016-12-26 01:11:37 -0500

    Gulp Workflow Image Optimization
    Installed imagemin and gulp-cache for image optimization. I created a
    task that will optimize images and compress them and send them into
    production

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,9 @@ var gulp = require('gulp'),
 	postcss = require('gulp-postcss'),
 	autoprefixer = require('autoprefixer'),
 	precss = require('precss'),
-	cssnano = require('cssnano');
+	cssnano = require('cssnano'),
+	imagemin = require('gulp-imagemin'),
+	cache = require('gulp-cache');
 
 // HTML
 gulp.task('html', function() {
@@ -37,12 +39,23 @@ gulp.task('css', function() {
     ]))
     .pipe(gulp.dest('dist/css'))
 });
+ 
+// Optimize Images
+gulp.task('images', function() {
+  gulp.src('development/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    // Caching images that ran through imagemin
+    .pipe(cache(imagemin({
+      	progressive: true, optimizationlevel: 7
+      })))
+    .pipe(gulp.dest('dist/images'))
+});
 
 // Watch Files for changes
 gulp.task('watch', function() {
 	gulp.watch('development/**/*.html')
 	gulp.watch('development/scss/**/*.scss')
 	gulp.watch('development/css/**/*.css')
+	gulp.watch('development/images/**/*.+(png|jpg|jpeg|gif|svg)')
 });
 
 // Development server to watch and update all HTML, CSS, images and js files
@@ -50,7 +63,7 @@ gulp.task('browser-sync', function () {
    var files = [
       'development/**/*.html',
       'development/css/**/*.css',
-      'development/images/**/*.png',
+      'development/images/**/*.+(png|jpg|jpeg|gif|svg)',
       'development/js/**/*.js'
    ];
 
@@ -61,4 +74,7 @@ gulp.task('browser-sync', function () {
    });
 });
 
-gulp.task('default', ['watch', 'html', 'sass', 'css']);
+gulp.task('default', ['watch', 'html', 'sass', 'css', 'images']);
+
+
+
