9829778102c80b6db52258ca237daec44f6a4301
Bryan Cash <bcash0578@gmail.com>
2016-12-28 13:32:21 -0500

    fixed typo in JSON file and added Grunt
    Fixed a typo in JSON file that was not allowing me to install grunt
    trailing comas on lines 16 and 23. Removed comas and installed grunt
    and grunt-responsive-images to create multiple image sizes for all
    images and copy them to the dist/images folder

---
+++ gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+// Initialize grunt configuration and set image sizes for responsive images
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 1600,
+            suffix: '_large_2x',
+            quality: 50
+          },{
+            width: 800,
+            suffix: '_large_1x',
+            quality: 50
+          },{
+            width: 750,
+            suffix: '_meduim',
+            quality: 50
+          },{
+            width: 500,
+            suffix: '_small',
+            quality: 50
+          }]
+        },
+//clear out images folder if it exists or create images folder for new responsive sized images
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'development/src_images/',
+          dest: 'development/images/'
+        }]
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          // includes files within path 
+          {expand: true, flatten: true, src: 'development/images/*', dest: 'dist/images/', filter: 'isFile'},
+        ],
+      },
+    },
+  });
+
+
+//load tasks
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  //run task
+  grunt.registerTask('default', ['responsive_images', 'copy']);
+
+};
